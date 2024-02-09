75315d3bb1675be4e01ff6d79a0ce78af5e9d24d
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-02-09 06:49:49 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int mat,fizik,turkce,kimya,muzik;
+        double ort;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Matematik notunuz: ");
+        mat = scanner.nextInt();
+
+        System.out.println("Fizik notunuz: ");
+        fizik = scanner.nextInt();
+
+        System.out.println("Turkce notunuz: ");
+        turkce = scanner.nextInt();
+
+        System.out.println("Kimya notunuz: ");
+        kimya = scanner.nextInt();
+
+        System.out.println("Muzik notunuz: ");
+        muzik = scanner.nextInt();
+
+        ort = (mat+fizik+turkce+ kimya+ muzik) /5;
+
+        if (ort <55){
+            System.out.println("naneyi yediniz");
+        }else {
+            System.out.println("tebrikler");
+        };
     }
 }
